14a547f72591418754b4b0767d8ae93f510bc13d
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-12-19 21:26:41 -0700

    javascript class oop solid

--- classes.js
+++ classes.js
@@ -3,13 +3,35 @@ class User {
         this.name = name
         this.site = 'it-incubator'
         this.dateOfBirth = dateOfBirth
+        this.counter = 0
     }
 
     hello() {
-        console.log(`My name is ${this.name} and i was born ${this.dateOfBirth}`)
+        this.counter++
+        console.log(`My name is ${this.name} and i was born ${this.dateOfBirth}. Counter : ${this.counter}`)
     }
 }
 
 const user1 = new User('Chel', '12.12.12')
-console.log(User)
 user1.hello()
+
+
+class Coder extends User {
+    constructor(name, dateOfBirth, tech) {
+        super(name, dateOfBirth);
+        this.tech = tech
+    }
+
+    code() {
+        console.log(`Im ${this.name}. This is my ${this.tech} code const sum=(a,b)=>a+b`)
+    }
+
+    hello() {
+        super.hello()
+        console.log('Go Away!')
+    }
+}
+
+const coder1 = new Coder('Zadr', '15,15,15', 'JS')
+coder1.code()
+coder1.hello()
